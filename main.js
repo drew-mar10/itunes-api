49263bd3da0678c2f36c04702ad9d7dfe82ba49e
Kalen Martin <kalen.martin1@gmail.com>
2022-02-13 11:47:42 -0500

    more structure

--- main.js
+++ main.js
@@ -2,48 +2,46 @@
 //                 body
 //                     div id form
 //                         _FORM_ search & results
-
 // Title
 //                 h2
 // (audio play bar)
 //                 div class player
 //                     audio player file
-
 // -separator-
-
 // (search bar w/ button)
 //                 div class search
 //                     search and button code
-
 // -separator-
-
 // search results
 //                 div class search results
 //                     undordered list
 // (songs & etc)
 
+//const searchDiv = document.querySelector('#music-search')
 
 //"https://itunes.apple.com/search?parameterkeyvalue&callback="{name of JavaScript function in webpage}"/>
 
 
-const form = document.querySelector('#search-form')
-const searchDiv = document.querySelector('#music-search')
-const url = ('https://itunes.apple.com/search?term=fleetwood+mac&limit=25')
+const form = document.getElementById('search-form')
+let url = "https://itunes.apple.com/search?term="
+const musicSearch = document.getElementById('music-search')
+const input = document.getElementsByClassName('inputInput')
 
 form.addEventListener("submit", function (event) {
     event.preventDefault()
     console.log("searched!")
+    console.log('value is: ', event.target)
 })
 
-fetch('https://itunes.apple.com/search?term=fleetwood+mac&limit=25')
-    .then(function(result){
-        console.log(result)
-        return result.json()
-})
-    .then (function(data){
-        console.log(data)
+// fetch('https://itunes.apple.com/search?term=fleetwood+mac&limit=25')
+//     .then(function(result){
+//         console.log(result)
+//         return result.json()
+// })
+//     .then (function(data){
+//         console.log(data)
 
-})
+// })
 
 
 
